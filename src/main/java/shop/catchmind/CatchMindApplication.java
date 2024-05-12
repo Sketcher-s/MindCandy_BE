a9a53322ffe6bcb13c596740823924b6a46973d1
Luke_YS <yjsmk0902@gmail.com>
2024-05-12 20:58:53 +0900

    :tada: Set up

---
+++ src/main/java/shop/catchmind/CatchMindApplication.java
@@ -0,0 +1,13 @@
+package shop.catchmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchMindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/catchmind/CatchMindApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.catchmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
