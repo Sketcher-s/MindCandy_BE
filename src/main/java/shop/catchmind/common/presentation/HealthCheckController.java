2af466e9a79c16734dc815249eee084bac521fce
Luke_YS <yjsmk0902@gmail.com>
2024-05-14 17:16:20 +0900

    :construction_worker: Init: HTTPS Health Check 응답 추가 (#1)

---
+++ src/main/java/shop/catchmind/common/presentation/HealthCheckController.java
@@ -0,0 +1,12 @@
+package shop.catchmind.common.presentation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public String health() {
+        return "OK!";
+    }
+}
