c489ec5652e900dc5efd4d448f27b9c4b253b3fa
Gyehwan Baek <b99hwan@gmail.com>
2024-05-17 19:25:42 +0900

    :sparkles: FEAT: GPT 연동 및 테스트

---
+++ src/main/java/shop/catchmind/global/config/RestTemplateConfig.java
@@ -0,0 +1,18 @@
+package shop.catchmind.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(60000);
+        requestFactory.setReadTimeout(60000);
+
+        return new RestTemplate(requestFactory);
+    }
+}
