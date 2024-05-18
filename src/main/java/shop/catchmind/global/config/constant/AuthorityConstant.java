3a99dc4fabce7971c1fd3460ab65fd16bf570db8
Luke_YS <yjsmk0902@gmail.com>
2024-05-18 17:32:55 +0900

    :sparkles: FEAT: 인증/인가 상수 추가 (#5)

---
+++ src/main/java/shop/catchmind/global/config/constant/AuthorityConstant.java
@@ -0,0 +1,8 @@
+package shop.catchmind.global.config.constant;
+
+public final class AuthorityConstant {
+
+    private AuthorityConstant() {}
+    public static final String USER = "ROLE_USER";
+
+}
