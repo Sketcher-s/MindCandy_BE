9d21b9fdb18d4abd8b54c4701e4d0297218bad4f
Luke_YS <yjsmk0902@gmail.com>
2024-05-18 17:33:38 +0900

    :sparkles: FEAT: 인증/인가 필터 추가 (#5)
    - JWT 인증 필터
    - Json 로그인 필터

---
+++ src/main/java/shop/catchmind/auth/filter/CustomJsonAuthenticationFilter.java
@@ -0,0 +1,45 @@
+package shop.catchmind.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static shop.catchmind.global.config.constant.AuthenticationProcessingConstant.*;
+
+public class CustomJsonAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    private final ObjectMapper objectMapper;
+
+    public CustomJsonAuthenticationFilter(final ObjectMapper objectMapper) {
+        super(DEFAULT_LOGIN_PATH_REQUEST_MATCHER);
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(final HttpServletRequest request, final HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        if (request.getContentType() == null || !request.getContentType().equals(CONTENT_TYPE)) {
+            throw new AuthenticationServiceException("Authentication Content-Type not supported: " + request.getContentType());
+        }
+
+        String messageBody = StreamUtils.copyToString(request.getInputStream(), UTF_8);
+
+        Map<String, String> usernamePasswordMap = objectMapper.readValue(messageBody, Map.class);
+
+        String userId = usernamePasswordMap.get(USERNAME_KEY);
+        String userPassword = usernamePasswordMap.get(PASSWORD_KEY);
+
+        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(userId, userPassword);
+
+        return this.getAuthenticationManager().authenticate(authRequest);    }
+}
---
+++ src/main/java/shop/catchmind/auth/filter/JwtAuthProcessingFilter.java
@@ -0,0 +1,64 @@
+package shop.catchmind.auth.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import shop.catchmind.auth.Role;
+import shop.catchmind.auth.dto.AuthenticationDto;
+import shop.catchmind.auth.provider.JwtProvider;
+import shop.catchmind.member.domain.Member;
+import shop.catchmind.member.repository.MemberRepository;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class JwtAuthProcessingFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+    private final MemberRepository memberRepository;
+
+    private final GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
+
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain filterChain) throws ServletException, IOException {
+        checkAccessTokenAndAuthentication(request, response, filterChain);
+    }
+
+    private void checkAccessTokenAndAuthentication(final HttpServletRequest request, final HttpServletResponse response, final FilterChain filterChain) throws ServletException, IOException {
+        jwtProvider.extractAccessToken(request)
+                .filter(jwtProvider::isTokenValid)
+                .flatMap(accessToken -> jwtProvider.extractId(accessToken)
+                        .flatMap(memberRepository::findById)).ifPresent(this::saveAuthentication);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private void saveAuthentication(final Member myMember) {
+        AuthenticationDto authInfoDto = AuthenticationDto.builder()
+                .id(myMember.getId())
+                .email(myMember.getEmail())
+                .authorities(
+                        Set.of(Role.USER).stream()
+                                .map(Role::getAuthority)
+                                .map(SimpleGrantedAuthority::new)
+                                .collect(Collectors.toUnmodifiableSet()))
+                .build();
+
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(authInfoDto, null,
+                        authoritiesMapper.mapAuthorities(authInfoDto.getAuthorities()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+}
