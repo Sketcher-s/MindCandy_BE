68dab48d6e5d57becb56a58b5374d40d3ea6562f
Luke_YS <yjsmk0902@gmail.com>
2024-05-18 17:37:32 +0900

    :sparkles: FEAT: MemberRepository 메소드 추가 (#5)
    - findByEmail

--- src/main/java/shop/catchmind/member/repository/MemberRepository.java
+++ src/main/java/shop/catchmind/member/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package shop.catchmind.member.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.catchmind.member.domain.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(final String email);
 }
