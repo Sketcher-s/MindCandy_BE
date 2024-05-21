771f5b260e60c2fc2690efb237521a9a64c1de35
Luke_YS <yjsmk0902@gmail.com>
2024-05-22 04:16:25 +0900

    :memo: CHORE: SwaggerConfig 수정 (#11)
    - Login 문서화
    - 로컬 서버 문서화 추가

--- src/main/java/shop/catchmind/global/config/SwaggerConfig.java
+++ src/main/java/shop/catchmind/global/config/SwaggerConfig.java
@@ -2,21 +2,40 @@ package shop.catchmind.global.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.*;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import shop.catchmind.auth.filter.CustomJsonAuthenticationFilter;
 
 import java.util.List;
+import java.util.Optional;
+
+import static shop.catchmind.auth.constant.AuthProcessingConstant.*;
 
 @Configuration
+@RequiredArgsConstructor
 public class SwaggerConfig {
 
+    private final ApplicationContext applicationContext;
     private final String securitySchemeName = "bearerAuth";
 
     @Bean
@@ -43,7 +62,45 @@ public class SwaggerConfig {
                                 )
                 )
                 .info(info)
-                .servers(List.of(prodServer));
+                .servers(List.of(prodServer, localServer));
+    }
+
+    @Bean
+    public OpenApiCustomizer springSecurityLoginEndpointCustomizer() {
+        FilterChainProxy filterChainProxy = applicationContext.getBean(
+                AbstractSecurityWebApplicationInitializer.DEFAULT_FILTER_NAME,
+                FilterChainProxy.class);
+        return openAPI -> {
+            for (SecurityFilterChain filterChain : filterChainProxy.getFilterChains()) {
+                Optional<CustomJsonAuthenticationFilter> optionalFilter =
+                        filterChain.getFilters().stream()
+                                .filter(CustomJsonAuthenticationFilter.class::isInstance)
+                                .map(CustomJsonAuthenticationFilter.class::cast)
+                                .findAny();
+                if (optionalFilter.isPresent()) {
+                    CustomJsonAuthenticationFilter filter = optionalFilter.get();
+                    Operation operation = new Operation();
+                    Schema<?> schema = new ObjectSchema()
+                            .addProperty(USERNAME_KEY, new StringSchema())
+                            .addProperty(PASSWORD_KEY, new StringSchema());
+                    RequestBody requestBody = new RequestBody().content(
+                            new Content().addMediaType(
+                                    org.springframework.http.MediaType.APPLICATION_JSON_VALUE,
+                                    new MediaType().schema(schema)));
+                    operation.requestBody(requestBody);
+                    ApiResponses apiResponses = new ApiResponses();
+                    apiResponses.addApiResponse(String.valueOf(HttpStatus.OK.value()),
+                            new ApiResponse().description(HttpStatus.OK.getReasonPhrase()));
+                    apiResponses.addApiResponse(String.valueOf(HttpStatus.BAD_REQUEST.value()),
+                            new ApiResponse().description(HttpStatus.BAD_REQUEST.getReasonPhrase()));
+                    operation.responses(apiResponses);
+                    operation.addTagsItem("Auth API");
+                    operation.summary("로그인").description("아이디와 비밀번호를 받아 로그인합니다.");
+                    PathItem pathItem = new PathItem().post(operation);
+                    openAPI.getPaths().addPathItem(DEFAULT_LOGIN_REQUEST_URL, pathItem);
+                }
+            }
+        };
     }
 
 }
