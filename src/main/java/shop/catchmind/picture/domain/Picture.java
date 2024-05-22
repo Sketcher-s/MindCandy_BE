b4a43801546bd19f1cde479e9061bf42f42aaec5
Luke_YS <yjsmk0902@gmail.com>
2024-05-23 02:40:54 +0900

    :recycle: REFACTOR: Picture 엔티티 필드 추가 (#13)

--- src/main/java/shop/catchmind/picture/domain/Picture.java
+++ src/main/java/shop/catchmind/picture/domain/Picture.java
@@ -1,11 +1,6 @@
 package shop.catchmind.picture.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,6 +16,9 @@ public class Picture extends AuditingField {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "member_id")
+    private Long memberId;
 
     @Column(name = "title", length = 15)
     private String title;
@@ -40,7 +38,8 @@ public class Picture extends AuditingField {
     }
 
     @Builder
-    public Picture(final String title, final String imageUrl, final String result) {
+    public Picture(Long memberId, String title, String imageUrl, String result) {
+        this.memberId = memberId;
         this.title = title;
         this.imageUrl = imageUrl;
         this.result = result;
