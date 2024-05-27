eeaf8c6bcaa49910ffaf1c64faa6eda8262c7827
Gyehwan Baek <b99hwan@gmail.com>
2024-05-27 23:02:27 +0900

    :sparkles: FEAT: 마이페이지 조회 (repository)

--- src/main/java/shop/catchmind/picture/repository/PictureRepository.java
+++ src/main/java/shop/catchmind/picture/repository/PictureRepository.java
@@ -1,9 +1,13 @@
 package shop.catchmind.picture.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.catchmind.picture.domain.Picture;
 
 public interface PictureRepository extends JpaRepository<Picture, Long> {
 
     void deleteAllByMemberId(Long memberId);
+
+    Page<Picture> findByMemberId(Long memberId, Pageable pageable);
 }
