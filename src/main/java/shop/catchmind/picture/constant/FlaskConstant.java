07f6736b2d449600415c9053b7e075ae45405792
Luke_YS <yjsmk0902@gmail.com>
2024-09-02 13:51:15 +0900

    :sparkles: FEAT: Flask 연결 관련 상수 추출 (#40)

---
+++ src/main/java/shop/catchmind/picture/constant/FlaskConstant.java
@@ -0,0 +1,10 @@
+package shop.catchmind.picture.constant;
+
+public final class FlaskConstant {
+
+    private FlaskConstant() {}
+
+    public static final String FILE_REQUEST = "file";
+    public static final String MODEL_TYPE_REQUEST = "model_type";
+    public static final String NUMBER_CHARACTER_PATTERN = "\\(.*?\\)";
+}
