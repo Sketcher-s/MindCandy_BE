a04105f72b5866c79e112e8f28a023c65ebab64f
Luke_YS <yjsmk0902@gmail.com>
2024-09-21 14:55:31 +0900

    :recycle: REFACTOR: 소요 시간 필드 변경 (#51)

--- src/main/java/shop/catchmind/picture/dto/request/InspectRequest.java
+++ src/main/java/shop/catchmind/picture/dto/request/InspectRequest.java
@@ -1,12 +1,14 @@
 package shop.catchmind.picture.dto.request;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import shop.catchmind.picture.dto.PictureRequestDto;
 
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 public record InspectRequest(
         List<PictureRequestDto> pictureRequestDtoList,
-        LocalDateTime requiredTime
+        @DateTimeFormat(pattern = "HH:mm")
+        LocalTime consumedTime
 ) {
 }
